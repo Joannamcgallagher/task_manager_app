fee979f28ebe49b5fcbda82ed54f1959bf92f13e
Joannamcgallagher <joannamcgallagher@gmail.com>
2024-01-22 20:38:11 +0000

    Add function for new task and create the form for same on add_task.html

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,5 +1,22 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     // sidenav initialization
     let sidenav = document.querySelectorAll(".sidenav");
     M.Sidenav.init(sidenav);
+});
+
+// datepicker initialization
+document.addEventListener('DOMContentLoaded', function () {
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+        format: "dd mmm, yyyy",
+        i18n: {
+            done: "Select"
+        }
+    });
+});
+
+// select initialization
+document.addEventListener('DOMContentLoaded', function () {
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
 });
